04f111904c40fffd7ee3f69ae3944c376256d008
thomasvanroost <thomasvanroost@gmail.com>
2015-08-19 13:27:33 +0200

    DB connection gemaakt! Geen connection string voorzien!

---
+++ HorecaVlaanderenOpleidingen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HorecaVlaanderenOpleidingen.Startup))]
+namespace HorecaVlaanderenOpleidingen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
